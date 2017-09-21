e6ce7844e238885108bf0f78d3e70a5c24b346bd
niloofardadras <niloodadrass@gmail.com>
2017-09-21 18:26:42 -0400

    Add files via upload

---
+++ patient registration.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : bme506_l3.cpp
+// Author      : niloofar hosseinidadras
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+	int main() {
+	double patient[20], *ptr= &patient[0];//defining array patient of size 20 and pointer that refers to address 0 of teh array
+	int i;
+
+	for (i=0;i<20;i++) //for loop to take in data and assign it to an address of patient
+	{
+	cout <<"Enter Patient: ";
+	cin >> *ptr;
+	ptr++;
+	}
+	/*for (i=0;i<20;i++)
+	{
+	cout<<patient[i]<<"\n";
+	}*/
+	for (i=0;i<20;++i)//function to calculate the new patient array by the formula given in the lab
+	{
+	patient[i]=patient[i]+patient[i]/(i+1);
+	cout<<"new patient is: "<<patient[i]<<"\n";
+	}
+	return 0;
+	}
